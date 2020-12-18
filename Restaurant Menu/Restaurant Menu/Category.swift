89dbf61c9778bea81362bbc3eaaf604f7a6536c8
Shimaa Hassan <eng.shimaa71296@gmail.com>
2020-12-19 00:11:42 +0200

    add category and product models

---
+++ Restaurant Menu/Restaurant Menu/Category.swift
@@ -0,0 +1,16 @@
+//
+//  Menu.swift
+//  Restaurant Menu
+//
+//  Created by Shimaa Hassan on 12/19/20.
+//  Copyright © 2020 Shimaa Hassan. All rights reserved.
+//
+
+import Foundation
+
+struct Category{
+    var id: Int?
+    var name: String?
+}
+
+
---
+++ Restaurant Menu/Restaurant Menu/Product.swift
@@ -0,0 +1,17 @@
+//
+//  Product.swift
+//  Restaurant Menu
+//
+//  Created by Shimaa Hassan on 12/19/20.
+//  Copyright © 2020 Shimaa Hassan. All rights reserved.
+//
+
+import Foundation
+
+struct Product {
+    var id: Int?
+    var name: String?
+    var category: Category?
+    var price: String?
+    var imageURL: String?
+}
